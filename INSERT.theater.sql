fd848c9a8d67b378914983d9b3d464502e58099b
Berik <bbaibaturov@gmail.com>
2023-11-07 10:36:23 -0500

    homework_wk5_d1

---
+++ INSERT.theater.sql
@@ -0,0 +1,25 @@
+INSERT INTO customer (
+    email,
+	phone,
+	address
+) VALUES (
+	'berikbbt@gmail.com',
+	'777-777-7777',
+	'30 Perot ave.'
+);
+
+SELECT *
+FROM customer;
+
+INSERT INTO ticket(
+	ticket_number,
+	seat,
+	cost,
+	quantity
+) VALUES (
+	'A1234',
+	'4333',
+	'75',
+	'3'
+);
+
